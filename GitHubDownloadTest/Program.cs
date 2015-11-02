f3026dbec39adb64f153e3505220218345e3ca84
pvaalu <pvaalu26@gmail.com>
2015-11-02 12:21:12 +0530

    initial comit

---
+++ GitHubDownloadTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubDownloadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("user-agent", "Anything");
+                client.DownloadFile(
+                    "https://api.github.com/repos/nodatime/nodatime/zipball",
+                    "nodatime.zip");
+            }
+        }
+    }
+}
